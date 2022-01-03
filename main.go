a5ec8d475a70d93fffb5926717096d79206d05c5
naupiimz <naupiimz@gmail.com>
2022-01-03 18:59:59 +0700

    3 jan 2022

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
 
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", helloHandler)
+
+	log.Println("Server is up and running")
+
+	err := http.ListenAndServe(":8080", mux)
+
+	log.Fatal(err)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
 }
